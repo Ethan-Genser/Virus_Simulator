335579081ba35ab018f0e707606068fe8c188dcd
Ethan Genser <ethanp1033@gmail.com>
2020-04-03 05:07:02 -0500

    chart is functional, simulation runs, start button partially implemented.

--- virusSim.js
+++ virusSim.js
@@ -7,16 +7,19 @@ var chartCanvas = document.getElementById("chartCanvas");
 var chartCtx = chartCanvas.getContext("2d");
 chartCtx.canvas.width = window.innerWidth/2;
 chartCtx.canvas.height = window.innerHeight/2;
+var uiCanvas = document.getElementById("uiCanvas");
+var uiCtx = uiCanvas.getContext("2d");
+uiCtx.canvas.width = window.innerWidth/2;
+uiCtx.canvas.height = window.innerHeight/2;
 
-var subjects = [];
-var numOfFamilies = 9;
-var population = 20;
+var numOfFamilies = 1;
+var population = 100;
 var commonZone = false;
 var infectivityRate = 0.1;
-var infectivityRadius = 10;
+var infectivityRadius = 20;
 var socialDistancing = 1.0;
 var interfamilyDistancing = 1.0;
-var duration = 10;
+var durationOfInfection = 15;
 
 var familyPadding = 0.1;
 var familyColumns = 0;
@@ -25,17 +28,64 @@ var familyColor = "#bbbbbb"
 var familySize = findFamilySize();
 var families = generateFamilies();
 
+var subjects = generateSubjects();
+var susceptibleColor = "#4bc0c0";
+var infectedColor = "#f57842";
+var removedColor = "#636363";
+var subjectRadius = 10;
+var subjectMaxVelocity = 1;
+
+var INFECTED = 1;
+var infectedPopulation = 0;
+var infectedData = [infectedPopulation];
+var SUSCEPTIBLE = 0;
+var susceptiblePopulation = population;
+var susceptibleData = [susceptiblePopulation];
+var REMOVED = 2;
+var removedPopulation = 0;
+var removedData = [removedPopulation];
+
+var timer = 0;
+var lastTime = Date.now();
+var seconds = 0;
+var simulationComplete = true;
+
+uiCanvas.addEventListener('click', function(event) {
+	var mousePosition = getMousePosition(uiCanvas, event);
+
+	if (isInside(mousePosition, startButton)) {
+		simulationComplete = false;
+		update();
+	}
+});
+var startButton = {x: uiCanvas.width/2 - 75, y: uiCanvas.height - 100, width: 150, height: 75, color: "#636363"}
+
 var chart = new Chart(chartCtx, {
 	"type": "line",
 	"data": {
-		"labels": [1,2,3,4,5,6,7],
+		"labels": [0],
 		"datasets": [{
-			"label": "My First Dataset",
-			"data": [65,59,80,81,56,55,40],
-			"fill": true,
-			"borderColor": "rgb(75, 192, 192)",
-			"lineTension": 0
-		}]
+				"label": "Healthy",
+				"data": [],
+				"fill": true,
+				"borderColor": susceptibleColor,
+				"lineTension": 0.1
+			},
+			{
+				"label": "Infected",
+				"data": [],
+				"fill": true,
+				"borderColor": infectedColor,
+				"lineTension": 0.1
+			},
+			{
+				"label": "Removed",
+				"data": [],
+				"fill": true,
+				"borderColor": removedColor,
+				"lineTension": 0.1
+			}
+		]
 	},
 	"options": {
 		responsive:false
@@ -85,23 +135,191 @@ function generateFamilies() {
 	return _families;
 }
 
-function drawRect(x, y, width, height, color) {
-	simCtx.beginPath();
-	simCtx.rect(x, y, width, height);
-	simCtx.fillStyle = color;
-	simCtx.fill();
-	simCtx.closePath();
+function generateSubjects() {
+	var _subjects = [];
+	for (var i = 0; i < population; i++) {
+		var family = Math.floor(Math.random() * numOfFamilies);
+		var posX = families[family].x + Math.floor(Math.random() * familySize);
+		var posY = families[family].y + Math.floor(Math.random() * familySize);
+		_subjects[i] = {x: posX, y: posY, isInfected: false, isRemoved: false, velX: 0.0, velY: 0.0, family: family, timeOfInfection: 0, drawnInfectionRadius: 0}
+		if (i == population-1) {
+			_subjects[i].isInfected = true;
+			_subjects[i].timeOfInfection = Date.now();
+			infectedPopulation++;
+			susceptiblePopulation--;
+		}
+	}
+
+	return _subjects;
 }
 
-function draw() {
+function updateSubjects() {
+	for (var i = 0; i < population; i++) {
+		var _family = families[subjects[i].family];
+
+		// Adds a random force to the subjects' current velocity
+		subjects[i].velX += (Math.floor(Math.random() * 3) - 1) * 0.1;
+		subjects[i].velY += (Math.floor(Math.random() * 3) - 1) * 0.1;
+
+		// Prevents subjects from moving outside their family
+		if (Math.abs(subjects[i].x - _family.x) < 10) {
+			subjects[i].velX = 1
+		}
+			
+		if (_family.x+familySize - subjects[i].x < 10) {
+			subjects[i].velX = -1;
+		}
+		if (Math.abs(subjects[i].y - _family.y) < 10) {
+			subjects[i].velY = 1;
+		}
+		if (_family.y+familySize - subjects[i].y < 10) {
+			subjects[i].velY = -1;
+		}
+
+		// Prevents subjects velocity from exceeding the maximum speed allowed
+		if (subjects[i].velX > subjectMaxVelocity) {
+			subjects[i].velX -= 0.1;
+		}
+		else if (subjects[i].velX < -subjectMaxVelocity) {
+			subjects[i].velX += 0.1;
+		}
+		if (subjects[i].velY > subjectMaxVelocity) {
+			subjects[i].velY -= 0.1;
+		}
+		else if (subjects[i].velY < -subjectMaxVelocity) {
+			subjects[i].velY += 0.1;
+		} 
+
+		// Applies subjects' velocity to move their position
+		subjects[i].x += subjects[i].velX;
+		subjects[i].y += subjects[i].velY;
+
+		// Checks to see if the infection has spread to the subject
+		if (!subjects[i].isInfected && !subjects[i].isRemoved) {
+			for (var j = 0; j < population; j++) {
+				if (subjects[j].isInfected) {
+					var distance = Math.sqrt(Math.pow(subjects[i].x - subjects[j].x, 2) + Math.pow(subjects[i].y - subjects[j].y, 2))
+					if (distance < infectivityRadius) {
+						if (infectivityRate > Math.random()) {
+							subjects[i].isInfected = true;
+							subjects[i].timeOfInfection = Date.now();
+							infectedPopulation++;
+							susceptiblePopulation--;
+						}
+					}
+				}
+			}
+		}
+
+		// Checks if an infected subject recovers
+		if (subjects[i].isInfected) {
+			subjects[i].drawnInfectionRadius += 0.1;
+			if (subjects[i].drawnInfectionRadius > infectivityRadius + subjectRadius/2) {
+				subjects[i].drawnInfectionRadius = subjectRadius/2;
+			}
+			if (Date.now() - subjects[i].timeOfInfection > durationOfInfection * 1000) {
+				subjects[i].isInfected = false;
+				subjects[i].isRemoved = true;
+				infectedPopulation--;
+				removedPopulation++;
+			}
+		}
+	}
+}
+
+function updateChart() {
+	susceptibleData.push(susceptiblePopulation);
+	infectedData.push(infectedPopulation);
+	removedData.push(removedPopulation);
+
+	timer += Date.now() - lastTime;
+	lastTime = Date.now();
+	if (timer > 1000) {
+		timer = 0;
+		seconds++;
+		chart.data.labels.push(seconds);
+		chart.data.datasets[SUSCEPTIBLE].data.push(susceptiblePopulation);
+		chart.data.datasets[INFECTED].data.push(infectedPopulation);
+		chart.data.datasets[REMOVED].data.push(removedPopulation);
+	}
+
+	chart.update();
+}
+
+function getMousePosition(canvas, event) {
+	var rectangle = canvas.getBoundingClientRect();
+    return {
+        x: event.clientX - rectangle.left,
+        y: event.clientY - rectangle.top
+    };
+}
+
+function isInside(position, rectangle){
+    return position.x > rectangle.x && position.x < rectangle.x+rectangle.width && position.y < rectangle.y+rectangle.height && position.y > rectangle.y
+}
+
+function drawRect(context, x, y, width, height, color) {
+	context.beginPath();
+	context.rect(x, y, width, height);
+	context.fillStyle = color;
+	context.fill();
+	context.closePath();
+}
+
+function drawCircle(context, x, y, radius, color, fill) {
+	context.beginPath();
+	context.arc(x, y, radius, 0, 2 * Math.PI);
+	fill ? simCtx.fillStyle = color : simCtx.strokeStyle = color;
+	fill ? simCtx.fill() : simCtx.stroke();
+	context.closePath();
+}
+
+function update() {
 	simCtx.clearRect(0, 0, simCanvas.width, simCanvas.height);
 
-	// Families
+	if (susceptiblePopulation < 0) {
+		susceptiblePopulation = 0;
+	}
+	if (infectedPopulation <= 5) {
+		var infectedcnt = 0;
+		for (var i = 0; i < population; i++) {
+			if (subjects[i].isInfected) {
+				infectedcnt++;
+			}
+		}
+		if (infectedcnt == 0) {
+			infectedPopulation = 0;
+			simulationComplete = true;
+		}
+	}
+
+	// Draw Families
 	for (var i = 0; i < numOfFamilies; i++) {
-		drawRect(families[i].x, families[i].y, families[i].width, families[i].height, familyColor);
+		drawRect(simCtx, families[i].x, families[i].y, families[i].width, families[i].height, familyColor);
 	}
 	
-	requestAnimationFrame(draw);
+	// Draw Subjects
+	for (var i = 0; i < population; i++) {
+		if (subjects[i].isRemoved) {
+			drawCircle(simCtx, subjects[i].x, subjects[i].y, subjectRadius, removedColor, true);
+		}
+		else if (subjects[i].isInfected) {
+			drawCircle(simCtx, subjects[i].x, subjects[i].y, subjectRadius, infectedColor, true);
+			drawCircle(simCtx, subjects[i].x, subjects[i].y, subjects[i].drawnInfectionRadius, infectedColor, false);
+		}
+		else {
+			drawCircle(simCtx, subjects[i].x, subjects[i].y, subjectRadius, susceptibleColor, true);
+		}
+	}
+
+	// Draw UI
+	drawRect(uiCtx, startButton.x, startButton.y, startButton.width, startButton.height, startButton.color);
+
+	if (!simulationComplete) {
+		updateSubjects();
+		updateChart();
+		requestAnimationFrame(update);
+	}
 }
 
-draw();
+update();
